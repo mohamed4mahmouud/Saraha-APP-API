9dd1d8d4f09313b2f37cba708c46e3f92be2ef05
mohamed4mahmouud <mohamedalamrawy55@gmail.com>
2024-02-07 18:58:27 +0200

    refactor user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,26 +10,14 @@ router.patch("/resetPassword", authController.resetPassword);
 router.post("/signup", authController.signUp);
 router.post("/login", authController.signIn);
 
-router.route("/confirmEmail/:token").get(authController.confirmEmail);
+router.get("/confirmEmail/:token", authController.confirmEmail);
 
-router
-  .route("/profile")
-  .get(authController.protect, userController.userProfile);
+router.use(authController.protect);
 
-router
-  .route("/messages")
-  .get(authController.protect, userController.getUserMessages);
-
-router
-  .route("/changePassword")
-  .patch(authController.protect, authController.changePassword);
-
-router
-  .route("/updateMe")
-  .patch(authController.protect, userController.updateMe);
-
-router
-  .route("/deleteMe")
-  .delete(authController.protect, userController.deleteUser);
+router.get("/profile", userController.userProfile);
+router.get("/messages", userController.getUserMessages);
+router.patch("/changePassword", authController.changePassword);
+router.patch("/updateMe", userController.updateMe);
+router.delete("/deleteMe", userController.deleteUser);
 
 export default router;
